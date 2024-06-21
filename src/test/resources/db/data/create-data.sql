440c084eb6c9ff8040f7a850d460d13e1243962a
hehmdalolkek <badeha.inna@yandex.ru>
2024-06-21 13:31:58 +0400

    deleted orders from project

--- src/test/resources/db/data/create-data.sql
+++ src/test/resources/db/data/create-data.sql
@@ -7,20 +7,3 @@ VALUES
     (5, 'Mango', 200.40, false);
 
 SELECT setval('products_product_id_seq', (SELECT MAX(product_id) FROM products) + 1);
-
-INSERT INTO public.orders (order_id, order_date, total_cost)
-VALUES
-    (1, '2024-10-06T10:20:10', 79.95),
-    (2, '2024-10-07T12:30:50', 159.23),
-    (3, '2024-10-08T13:40:20', 60);
-
-SELECT setval('orders_order_id_seq', (SELECT MAX(order_id) FROM orders) + 1);
-
-INSERT INTO public.orders_products (order_product_id, order_id, product_id, amount)
-VALUES
-    (1, 1, 1, 5),
-    (2, 2, 2, 2),
-    (3, 2, 1, 3),
-    (4, 3, 2, 2);
-
-SELECT setval('orders_products_order_product_id_seq', (SELECT MAX(order_product_id) FROM orders_products) + 1);
--- src/test/resources/db/data/delete-data.sql
+++ src/test/resources/db/data/delete-data.sql
@@ -1,3 +1,1 @@
-DELETE FROM public.orders_products;
-DELETE FROM public.orders;
 DELETE FROM public.products;
