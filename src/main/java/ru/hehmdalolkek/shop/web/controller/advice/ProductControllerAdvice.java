9a11c3fc938bbe1129096c60d194fff9cff3d178
hehmdalolkek <badeha.inna@yandex.ru>
2024-06-24 13:21:52 +0400

    added product controller advice with custom exceptions handler

---
+++ src/main/java/ru/hehmdalolkek/shop/web/controller/advice/ProductControllerAdvice.java
@@ -0,0 +1,37 @@
+package ru.hehmdalolkek.shop.web.controller.advice;
+
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import ru.hehmdalolkek.shop.model.exception.ProductIsAlreadyExistsException;
+import ru.hehmdalolkek.shop.model.exception.ProductNotFoundException;
+
+@ControllerAdvice
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class ProductControllerAdvice {
+
+    @ExceptionHandler(ProductNotFoundException.class)
+    public ResponseEntity<?> handleProductNotFoundException(ProductNotFoundException e) {
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(
+                HttpStatus.NOT_FOUND, e.getMessage()
+        );
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(problemDetail);
+    }
+
+    @ExceptionHandler(ProductIsAlreadyExistsException.class)
+    public ResponseEntity<?> handleProductIsAlreadyExistsException(ProductIsAlreadyExistsException e) {
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(
+                HttpStatus.CONFLICT, e.getMessage()
+        );
+        return ResponseEntity
+                .status(HttpStatus.CONFLICT)
+                .body(problemDetail);
+    }
+
+}
