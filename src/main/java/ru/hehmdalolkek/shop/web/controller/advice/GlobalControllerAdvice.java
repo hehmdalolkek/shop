802e0b3951735107eebbbf24524972f5d6abdcdb
hehmdalolkek <badeha.inna@yandex.ru>
2024-06-24 13:22:40 +0400

    added global controller advice with standard exceptions handler

---
+++ src/main/java/ru/hehmdalolkek/shop/web/controller/advice/GlobalControllerAdvice.java
@@ -0,0 +1,32 @@
+package ru.hehmdalolkek.shop.web.controller.advice;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
+
+@ControllerAdvice
+public class GlobalControllerAdvice {
+
+    @ExceptionHandler(NoResourceFoundException.class)
+    public ResponseEntity<?> handleResourceNotFoundException(NoResourceFoundException e) {
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(
+                HttpStatus.NOT_FOUND, e.getMessage()
+        );
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(problemDetail);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<?> handleException(Exception e) {
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(
+                HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage()
+        );
+        return ResponseEntity.internalServerError()
+                .body(problemDetail);
+    }
+
+}
